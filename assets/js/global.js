76ce3e356bccd2c7e56f1fd0c436ce860d5e1d0c
Antonis Galanis <ant.galanis@gladd.gr>
2020-09-25 20:08:25 +0300

    page 3

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,8 +1,27 @@
 $('.bibliografia').on($LEAP.ie_start, function() {
-    console.log('test');
     $(this).toggleClass('open');
 })
 
 $('.home-btn').on($LEAP.ie_start, function() {
     $LEAP.slideName('page_01');
+})
+
+$('.side-drawer .toggle').on($LEAP.ie_start, function() {
+    var isOpen = $(this).closest('.side-drawer').hasClass('open');
+    $('.side-drawer').removeClass('open');
+    if(!isOpen) {
+        $(this).closest('.side-drawer').addClass('open');
+    }
+})
+
+$(document).on($LEAP.ie_start, function(e) {
+    if ($(e.target).closest('.side-drawer').length == 0) {
+        $('.side-drawer').removeClass('open');
+    }
+})
+
+$(document).on($LEAP.ie_start, function(e) {
+    if ($(e.target).closest('.bibliografia').length == 0) {
+        $('.bibliografia').removeClass('open');
+    }
 })
